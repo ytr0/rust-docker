928795a1c421cb6acbb29b54ff3bf505211adba9
ytr0 <yu20@iamas.ac.jp>
2024-04-03 05:18:42 +0900

    first commit

---
+++ backend/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# cargo new hello_cargo
+# cd hello_cargo
+# cargo run
+
+rustc /app/src/ch1/hello.rs
+
+./hello
---
+++ backend/src/ch1/hello.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let x = 5 * 5;
+    println!("Hello, World! {}", x);
+}
