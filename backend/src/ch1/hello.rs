7becda25b80c13332e4337b91f1b64744aa77d0e
ytr0 <yu20@iamas.ac.jp>
2024-04-13 18:43:16 +0900

    Add musl-tools and cargo-watch to Dockerfile, update hello.rs

--- backend/src/ch1/hello.rs
+++ backend/src/ch1/hello.rs
@@ -1,4 +1,17 @@
+fn g(t: &str) {
+    println!("g: {t}");
+}
+
+fn f() {
+    let mut s = String::from("Hey"); 
+    let t = &s;
+    g(&s);
+    s.push_str(" there!");
+    // println!("f: {s} {t}");
+}
+
 fn main() {
+    f();
     let x = 5 * 5;
     println!("Hello, World! {}", x);
 }
